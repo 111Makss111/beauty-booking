ad289f1d5f2d4b9d75122ae7d193884297ed7734
777Makss777 <777docmax777@gmail.com>
2026-02-28 09:55:58 +0100

    fix: add empty page modules for admin and klient to fix build error

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function AdminDashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-slate-800">
+        Кабінет Адміністратора (в розробці)
+      </h1>
+    </div>
+  );
+}
